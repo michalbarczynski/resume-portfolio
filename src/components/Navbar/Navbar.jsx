7c2bb0c655215ff7130de1de4a3f0a9d139f4e5e
Michał Barczyński <michal.barczynski97@gmail.com>
2024-06-30 15:48:32 +0200

    finish basic nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import "./Navbar.scss";
 
+
 const Navbar = () => {
   return (
     <>
       <nav className='nav-wrapper'>
         <div className='nav-content'>
-            <img className='logo' alt="./assets/images/logo.svg" />
+            <img className='logo' src="./assets/images/logo.png" alt="" />
                 <ul>
                     <li>
                         <a className='menu-item'>Home</a>
                     </li>
                     <li>
                         <a className='menu-item'>Skills</a>
-                    </li>
+                    </li> 
                     <li>
                         <a className='menu-item'>Work-Experience</a>
                     </li>
@@ -24,6 +25,9 @@ const Navbar = () => {
                         Hire Me
                     </button>
                 </ul>
+                <button className='menu-btn' onClick={() => {}}>
+                  <i className="fas fa-bars" style={{ fontSize: "1.8rem" }}></i>
+                </button>
         </div>
       </nav>
     </>
