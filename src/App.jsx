f375ada69cbea543e9927c6e493d8bc251138832
Michał Barczyński <michal.barczynski97@gmail.com>
2024-07-06 16:08:34 +0200

    add skillscard and mergr hero&master

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from "./components/Navbar/Navbar"
+import Skills from "./components/Skills/Skills"
 
 function App() {
   return (
     <>
       <Navbar/>
+      <Skills/>
     </>
   );
 }
