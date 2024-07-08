2acf784bc2debaffb7cab8f8f9cc8c3c2110fa4c
Michał Barczyński <michal.barczynski97@gmail.com>
2024-07-08 22:00:31 +0200

    finish WE section without testing

--- src/components/Work Experience/WorkExperience.jsx
+++ src/components/Work Experience/WorkExperience.jsx
@@ -21,12 +21,26 @@ const WorkExperience = () => {
         },
       },
     ],
+  };
+
+  const slideRight = () => {
+    slideRef.current.slickNext();
+  }
+
+  const slideLeft = () => {
+    slideRef.current.slickPrev();
   }
 
   return (
     <section className="experience-container">
         <h5>Work Experience</h5>
         <div className="experience-content">
+          <div className="arrow-right" onClick={slideRight}>
+            <span class="material-symbols-outlined">chevron_right</span>
+          </div>
+          <div className="arrow-left" onClick={slideLeft}>
+            <span class="material-symbols-outlined">chevron_left</span>
+          </div>
           <Slider ref={slideRef} {...settings}>
             {WORK_EXPERIENCE.map(item => (
               <ExperienceCard key={item.title} details={item} />
