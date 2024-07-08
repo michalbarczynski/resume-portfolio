13406e0bd9e99eed7da3ed460072ab8c3a78deb9
Michał Barczyński <michal.barczynski97@gmail.com>
2024-07-08 22:12:53 +0200

    update database

--- src/utils/skillsAndWorkExperienceData.js
+++ src/utils/skillsAndWorkExperienceData.js
@@ -49,11 +49,51 @@ export const SKILLS = [
 
 export const WORK_EXPERIENCE = [
     {
-        title: "Frontend Developer",
-        date: "January 2020 - Present",
+        title: "Junior Frontend Developer",
+        date: "April 2023 - December 2023",
         responsibilities: [
-            "Lorep impsun",
-            "Dolor Sit Amet",
+            "Software development of cloud-based promotion management product",
+            "Fixing bugs created at successive stages of application development",
+            "Code implementation based on the SAP UI5 framework",
+            "Managing small team workflow as Agile Leader"
+        ],
+    },
+    {
+        title: "Junior Frontend Shopify Developer",
+        date: "December 2022 - March 2023",
+        responsibilities: [
+            "Resolving layout and responsiveness bugs",
+            "Transferring stores to the shopify platform",
+            "Development of e-commerce store software components"
+        ],
+    },
+    {
+        title: "QA Test Associate",
+        date: "March 2022 - November 2022",
+        responsibilities: [
+            "Testing video game-related software",
+            "Reporting bugs via Jira",
+            "Performing tasks from developers based on DevTrack tool",
+            "Verifying developers' work, by checking whether the bugs have been fixed correctly• Testing video game-related software. • Reporting bugs via Jira • Performing tasks from developers based on DevTrack tool • Verifying developers' work, by checking whether the bugs have been fixed correctly"
+        ],
+    },
+    {
+        title: "Shift Manager",
+        date: "August 2021 - January 2022",
+        responsibilities: [
+            "Coordinating the deliveries of products and being responsible about their placement in dark stores.",
+            "Managing a small group of employees - order pickers and drivers",
+            "Administration of dark store documentation",
+        ],
+    },
+    {
+        title: "Sales Administration Assistant",
+        date: "August 2020 - July 2021",
+        responsibilities: [
+            "Creating Credit Notes for European clients",
+            "After-sales documents management",
+            "Analysis of customer complaints",
+            "Cooperating with other departments and solving their current requests"
         ],
     },
 ];
