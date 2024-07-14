e9ad79cbc029347bcce36cfccea45b8904849ed5
Michał Barczyński <michal.barczynski97@gmail.com>
2024-07-14 16:11:33 +0200

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from './components/Hero/Hero';
 import './App.css'
 import WorkExperience from './components/Work Experience/WorkExperience';
 import ContactMe from './components/Contact/ContactMe';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <WorkExperience />
         <ContactMe />
       </div>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Footer.scss'
+
+const Footer = () => {
+  return (
+    <div className='footer'>© 2024 Michał Barczyński</div>
+  )
+}
+
+export default Footer 
