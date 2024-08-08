b8b35bd4669c7c80558a6886595edaee9e68b7c0
Michał Barczyński <michal.barczynski97@gmail.com>
2024-08-08 18:38:25 +0200

    redirect test

--- src/components/Contact/ContactForm/ContactForm.jsx
+++ src/components/Contact/ContactForm/ContactForm.jsx
@@ -4,18 +4,23 @@ import "./ContactForm.scss";
 const ContactForm = () => {
   return (
     <div className="contact-form-content">
-      <form action="https://formsubmit.co/michal.barczynski97@gmail.com" method="POST">
+      <form
+        action="https://formsubmit.co/michal.barczynski97@gmail.com"
+        method="POST"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <div className="name-container">
           <input type="text" name="firstname" placeholder="First Name" />
           <input type="text" name="lastname" placeholder="Last Name" />
         </div>
         <input type="text" name="email" placeholder="E-mail" />
-        <textarea type="text" name="message" placeholder="Message" rows={3} />
-        <input name="_redirect" type="hidden" id="name" value="https://google.com"/><input name="_formsubmit_id" type="text" style={{ display: 'none' }}/>
+        <textarea name="message" placeholder="Message" rows={3} />
+        <input type="hidden" name="_next" value="https://yourdomain.co/thanks.html"></input>
         <button type="submit">SEND</button>
       </form>
     </div>
   );
-};
+};  
 
 export default ContactForm;
